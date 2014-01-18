4cab4255a1c06203ea584b60427f035621ba6fa6
Ranveer Aggarwal <ranveeraggarwal@gmail.com>
2014-01-19 05:15:12 +0530

    Added canvas.js

--- Kapi/js/canvas.js
+++ Kapi/js/canvas.js
@@ -2,7 +2,7 @@
     pasteHtmlAtCaretUnselect("<div id='currcanvasdiv'><canvas id='paint' style='border:1px solid black;'></canvas></div>")
     ediv = document.getElementById("editdiv");
     ediv.contentEditable = false;
-    ediv.ondblclick = exitCanvas();
+    ediv.ondblclick = exitCanvas;
 }
 
 function exitCanvas() {
