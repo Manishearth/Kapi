2c3eebdaac7e4ff6994a604e3b26b48e7c8feb77
mihirk1994 <mihirk.1994@gmail.com>
2014-01-19 15:31:44 +0530

    Included file_open.js

---
+++ Kapi/js/file_open.js
@@ -0,0 +1,29 @@
+﻿function openbutton() {
+    // Verify that we are currently not snapped, or that we can unsnap to open the picker
+    var currentState = Windows.UI.ViewManagement.ApplicationView.value;
+    if (currentState === Windows.UI.ViewManagement.ApplicationViewState.snapped &&
+        !Windows.UI.ViewManagement.ApplicationView.tryUnsnap()) {
+        // Fail silently if we can't unsnap
+        return;
+    }
+
+    // Create the picker object and set options
+    var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+    openPicker.viewMode = Windows.Storage.Pickers.PickerViewMode.thumbnail;
+    openPicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.picturesLibrary;
+    // Users expect to have a filtered view of their folders depending on the scenario.
+    // For example, when choosing a documents folder, restrict the filetypes to documents for your application.
+    openPicker.fileTypeFilter.replaceAll([".kapi"]);
+
+    // Open the picker for the user to pick a file
+    openPicker.pickSingleFileAsync().then(function (file) {
+        if (file) {
+            // Application now has read/write access to the picked file
+            WinJS.log && WinJS.log("Picked file: " + file.name, "sample", "status");
+        } else {
+            // The picker was dismissed with no selected file
+            WinJS.log && WinJS.log("Operation cancelled.", "sample", "status");
+        }
+    });
+
+}
