afd3b2c4e14d6caafe4d373920b45745dbde62eb
Manish Goregaokar <manishsmail@gmail.com>
2014-01-19 10:10:32 +0530

    export

--- Kapi/js/file.js
+++ Kapi/js/file.js
@@ -47,7 +47,43 @@
         html2canvas(document.getElementById('editdiv'), {
             onrendered: function (canvas) {
                
-                Canvas2Image.saveAsPNG(canvas);
+                data = canvas.toDataURL()
+                data = data.split(";base64,")[1]
+                var imgData = Windows.Security.Cryptography.CryptographicBuffer.decodeFromBase64String(data);
+                var currentState = Windows.UI.ViewManagement.ApplicationView.value;
+                if (currentState === Windows.UI.ViewManagement.ApplicationViewState.snapped &&
+                    !Windows.UI.ViewManagement.ApplicationView.tryUnsnap()) {
+                    // Fail silently if we can't unsnap
+                    return;
+                }
+                // Create the picker object and set options
+                var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+                savePicker.suggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.documentsLibrary;
+                // Dropdown of file types the user can save the file as
+                savePicker.fileTypeChoices.insert("PNG Image", [".png"]);
+                // Default file name if the user does not type one in or select a file to replace
+                savePicker.suggestedFileName = "Newnote.png";
+
+                savePicker.pickSaveFileAsync().then(function (file) {
+                    if (file) {
+                        // Prevent updates to the remote version of the file until we finish making changes and call CompleteUpdatesAsync.
+                        Windows.Storage.CachedFileManager.deferUpdates(file);
+                        // write to file
+                        Windows.Storage.FileIO.writeBufferAsync(file, imgData).done(function () {
+                            // Let Windows know that we're finished changing the file so the other app can update the remote version of the file.
+                            // Completing updates may require Windows to ask for user input.
+                            Windows.Storage.CachedFileManager.completeUpdatesAsync(file).done(function (updateStatus) {
+                                if (updateStatus === Windows.Storage.Provider.FileUpdateStatus.complete) {
+                                    WinJS.log && WinJS.log("File " + file.name + " was saved.", "sample", "status");
+                                } else {
+                                    WinJS.log && WinJS.log("File " + file.name + " couldn't be saved.", "sample", "status");
+                                }
+                            });
+                        });
+                    } else {
+                        WinJS.log && WinJS.log("Operation cancelled.", "sample", "status");
+                    }
+                });
             }
         });
 
